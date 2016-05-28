b4484b6464a819cba02221dec7ec3b863de393ea
AntonioND <antonio_nd@outlook.com>
2016-05-28 19:16:19 +0200

    Preliminary pollution minimap

--- source/simulation/simulation_pollution.asm
+++ source/simulation/simulation_pollution.asm
@@ -33,9 +33,18 @@
 
 ;-------------------------------------------------------------------------------
 
+Simulation_PollutionDiffuminate:
+
+    ; Valid pollution values: -128 to 127
+
+    ; TODO
+
+    ret
+
+;-------------------------------------------------------------------------------
+
 ; Output data to WRAMX bank BANK_SCRATCH_RAM
 Simulation_Pollution::
-    ret ; TODO delete
 
     ; Clean
     ; -----
@@ -50,39 +59,71 @@ Simulation_Pollution::
 
     ; Valid pollution values: -128 to 127
 
-    ld      d,0 ; y
-.loopy:
-        ld      e,0 ; x
-.loopx:
-        push    de
+    ld      hl,CITY_MAP_TILES
+
+.loop:
+    push    hl
 
         ; Get tile type.
-        ; - If road, check traffic.
-        ; - If building, check if the building has power and add pollution if
-        ;   so. If it is a power plant, add the corresponding pollution level.
+        ; - If road, check traffic. Train doesn't pollute as it is electric.
+        ; - If building, check if the building has power and add pollution
+        ;   if so. If it is a power plant, add the corresponding pollution
+        ;   level.
         ; - If park, forest or water set a negative level of pollution (they
         ;   reduce it)
 
-        ; TODO
+        ld      a,BANK_CITY_MAP_TYPE
+        ld      [rSVBK],a
 
-        pop     de
+        ld      a,[hl]
 
-        inc     e
-        ld      a,CITY_MAP_WIDTH
-        cp      a,e
-        jr      nz,.loopx
+        bit     TYPE_HAS_ROAD_BIT,a
+        jr      z,.not_road
 
-    inc     d
-    ld      a,CITY_MAP_HEIGHT
-    cp      a,d
-    jr      nz,.loopy
+            ld      a,BANK_CITY_MAP_TRAFFIC
+            ld      [rSVBK],a
+
+            ld      b,[hl]
+
+            sra     b ; 0-255 to 0-127
+            ; Pollution is the amount of cars going through here
+
+            jr      .save_value
+
+.not_road:
+
+        ; Read pollution level array
+
+        push    hl
+        call    CityMapGetTileAtAddress ; hl = address, returns de = tile
+        call    CityTilePollution ; de = tile, returns d=pollution
+        pop     hl
+
+        ld      b,d
+
+;        jr      .save_value
+
+.save_value:
+
+        ld      a,BANK_SCRATCH_RAM
+        ld      [rSVBK],a
+
+        ld      [hl],b
+
+    pop     hl
+
+    inc     hl
+
+    bit     5,h ; Up to E000
+    jr      z,.loop
 
     ; Smooth map
     ; ----------
 
     ; Valid pollution values: -128 to 127
 
-    ; TODO
+    call    Simulation_PollutionDiffuminate
+    call    Simulation_PollutionDiffuminate
 
     ; Set all tiles with negative pollution value to 0
     ; ------------------------------------------------
@@ -91,7 +132,23 @@ Simulation_Pollution::
 
     ; Also, duplicate the values to go from 0-127 to 0-255
 
-    ; TODO
+    ld      hl,CITY_MAP_TILES
+
+    ld      a,BANK_SCRATCH_RAM
+    ld      [rSVBK],a
+
+.loop_fix:
+        ld      a,[hl]
+        bit     7,a
+        jr      z,.positive
+        xor     a,a ; if negative, 0
+.positive:
+        sla     a ; 0 to 127 -> 0 to 255
+
+        ld      [hl+],a
+
+    bit     5,h ; Up to E000
+    jr      z,.loop_fix
 
     ; Valid pollution values: 0 to 255
 
