943d81419f98aba4f1d6139a40cabbd6181ca4f9
AntonioND <antonio_nd@outlook.com>
2016-06-15 23:23:15 +0100

    Initial version of building growth
    Small buildings aren't removed to form big ones yet.

--- source/simulation/simulation_create_buildings.asm
+++ source/simulation/simulation_create_buildings.asm
@@ -27,6 +27,7 @@
 
     INCLUDE "room_game.inc"
     INCLUDE "tileset_info.inc"
+    INCLUDE "building_info.inc"
 
 ;###############################################################################
 
@@ -142,12 +143,203 @@ Simulation_FlagCreateBuildings::
 ;-------------------------------------------------------------------------------
 
 ; Try to build a building as big as possible.
+
 ; coords = bc
-; tile = de | only the low byte is needed
+; tile = de | only the low byte is needed because d should be 0 from the caller
 ; address = hl
 Simulation_CreateBuildingsTryBuild::
 
-    ; TODO
+    ; The tiles to test are arranged like this (0 is the origin):
+
+    ; 0 1 2
+    ; 3 4 5
+    ; 6 7 8
+
+    ; Check if all tiles are the same tile as register DE and if they are
+    ; flagged to build. D is 0, so we only need to make sure that each other
+    ; tile is < 256 and if E is the same as the lower byte. Any tile outside
+    ; the map makes the function to fail.
+
+    ; 1. Set size to 3x3.
+    ; 2a. Check coordinates to see if 3x3 fits.
+    ; 2b. Check 8, 7, 5, 6, 2. If any of them fails, fall back to 2x2.
+    ; 3a. Check coordinates to see if 2x2 fits.
+    ; 3b. Check 4, 3, 1. If they fail, fall back to 1x1.
+    ; 4. Build building.
+
+    add     sp,-1
+    ld      hl,sp+0
+    ld      [hl],e ; (*) save tile into stack
+
+    LD_DE_BC
+
+    ; [sp+0] = low byte of tile (high byte should be 0)
+    ; d = y, e = x
+    ; hl = address
+
+    ; 2a. Check coordinates to see if 3x3 fits
+
+    ld      a,61
+    cp      a,d ; carry flag is set if d > a (62 or 63)
+    jp      c,.check2x2
+    ld      a,61
+    cp      a,e ; carry flag is set if d > a (62 or 63)
+    jp      c,.check2x2
+
+    ; 2b. Check 8, 7, 5, 6, 2. If any of them fails, fall back to 2x2.
+
+START_POS_TEST : MACRO
+    push    de
+ENDM
+
+END_POS_TEST : MACRO ; 1 = jump here if failed
+        call    CityMapGetTileNoBoundCheck ; coords=de, returns tile=de
+        LD_BC_DE ; bc = tile
+    pop     de
+
+    xor     a,a
+    cp      a,b ; high byte should be 0!
+    jp      nz,\1
+
+    ld      hl,sp+2
+    ld      a,[hl]
+    cp      a,c ; low byte should be the same!
+    jp      nz,\1
+ENDM
+
+    ; d = y, e = x
+
+    START_POS_TEST ; Check 8
+        inc     e
+        inc     e
+        inc     d
+        inc     d
+    END_POS_TEST    .check2x2
+
+    START_POS_TEST ; Check 7
+        inc     e
+        inc     d
+        inc     d
+    END_POS_TEST    .check2x2
+
+    START_POS_TEST ; Check 5
+        inc     e
+        inc     e
+        inc     d
+    END_POS_TEST    .check2x2
+
+    START_POS_TEST ; Check 6
+        inc     d
+        inc     d
+    END_POS_TEST    .check2x2
+
+    START_POS_TEST ; Check 2
+        inc     e
+        inc     e
+    END_POS_TEST    .check2x2
+
+    START_POS_TEST ; Check 4
+        inc     e
+        inc     d
+    END_POS_TEST    .check2x2
+
+    START_POS_TEST ; Check 3
+        inc     d
+    END_POS_TEST    .check2x2
+
+    START_POS_TEST ; Check 1
+        inc     e
+    END_POS_TEST    .check2x2
+
+    jr      .build3x3
+
+.check2x2:
+
+    ; 3a. Check coordinates to see if 2x2 fits.
+
+    ld      a,62
+    cp      a,d ; carry flag is set if d > a (63)
+    jp      c,.build1x1
+    ld      a,62
+    cp      a,e ; carry flag is set if d > a (63)
+    jp      c,.build1x1
+
+    ; 3b. Check 4, 3, 1. If they fail, fall back to 1x1.
+
+    START_POS_TEST ; Check 4
+        inc     e
+        inc     d
+    END_POS_TEST    .build1x1
+
+    START_POS_TEST ; Check 3
+        inc     d
+    END_POS_TEST    .build1x1
+
+    START_POS_TEST ; Check 1
+        inc     e
+    END_POS_TEST    .build1x1
+
+    ;jr      .build2x2
+
+    ; Get offset
+.build2x2:
+    ld      b,B_ResidentialS2A - B_ResidentialS1A
+    jr      .build_end
+.build3x3:
+    ld      b,B_ResidentialS3A - B_ResidentialS1A
+    jr      .build_end
+.build1x1:
+    ld      b,B_ResidentialS1A - B_ResidentialS1A
+    ;jr      .build_end
+.build_end:
+
+    ld      hl,sp+0
+    ld      a,[hl]
+
+    add     sp,+1 ; (*) restore stack
+
+    ; a = RCI tile low byte
+    ; b = building size offset
+    ; de = origin coordinates
+
+    cp      a,T_RESIDENTIAL & $FF
+    jr      z,.res
+    cp      a,T_COMMERCIAL & $FF
+    jr      z,.com
+    cp      a,T_INDUSTRIAL & $FF
+    jr      z,.ind
+
+    ld      b,b ; Panic!
+    ret
+
+.res:
+    ld      a,B_ResidentialS1A
+    add     a,b
+    jr      .build
+.com:
+    ld      a,B_CommercialS1A
+    add     a,b
+    jr      .build
+.ind:
+    ld      a,B_IndustrialS1A
+    add     a,b
+    ;jr      .build
+
+    ; a = building size + type
+.build:
+
+    ld      b,a
+    call    GetRandom ; bc and de preserved
+    and     a,3
+    add     a,b ; randomize building type
+
+    ; TODO - Instead of randomizing, check demand? Maybe not a good idea
+    ; because it would reduce the variety.
+
+    ld      b,a
+    ; de = origin coordinates
+    ; b = building index
+    LONG_CALL_ARGS  MapDrawBuildingForcedCoords
 
     ret
 
@@ -158,8 +350,6 @@ Simulation_CreateBuildingsTryBuild::
 
 ; Don't update VRAM map, let the animation loop do that for us
 Simulation_CreateBuildings::
-
-    ret ; TODO - Remove this
 
     ; TODO - Actually use the city statistics (RCI demand) to affect the
     ; creation or destruction of buildings
@@ -234,6 +424,9 @@ ENDC
                     ; tile = de | only the low byte is needed, d is 0
                     ; hl = address
                     call    Simulation_CreateBuildingsTryBuild
+
+                    ; TODO - Try to build even if there are small buildings
+                    ; on the way!
 .not_build:
 
 .not_type_rci:
