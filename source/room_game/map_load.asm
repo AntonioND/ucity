AntonioND/ucity

commit f9fb80f2f3f2eccca37fbe4f5ee32b84c5ae2b67

Fix length of city name string

source/room_game/map_load.asm
@@ -38,7 +38,7 @@
 
 ;-------------------------------------------------------------------------------
 
-current_city_name:: DS TEXT_PROMPT_STRING_LENGHT+1 ; lenght + 0 terminator
+current_city_name:: DS TEXT_INPUT_LENGHT+1 ; lenght + 0 terminator
 
 selected_map: DS 1
 

commit 0bcebfa853b31f85dc4e2c4023e68078c077a4c9

Fix save data corruption
Bug introduced in f9fb80f2f3f2eccca37fbe4f5ee32b84c5ae2b67.

source/room_game/map_load.asm
@@ -380,7 +380,7 @@ SRAMMapLoad: ; a = index to load from. This function doesn't check bank limits.
     ld      hl,SAV_PERSISTENT_MSG
     call    PersistentMessageDataLoadFrom ; hl = src
 
-    ld      bc,TEXT_PROMPT_STRING_LENGTH
+    ld      bc,TEXT_INPUT_LENGTH
     ld      hl,SAV_CITY_NAME ; src
     ld      de,current_city_name ; dst
     call    memcopy