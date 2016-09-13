8a2c616532b6f2681bf81c0f4afc9463ec401857
AntonioND <antonio_nd@outlook.com>
2016-09-13 23:39:02 +0100

    Calculate circles of different radiuses
    Optimized code to calculate if a pair of coordinates is inside a
    circle of certain sizes (powers of two from 4 to 64). It needs a lot
    of space because of the LUTs used for it.

--- source/room_gen_map/gen_map.asm
+++ source/room_gen_map/gen_map.asm
@@ -31,7 +31,104 @@ seedw: DS 1
 
 ;###############################################################################
 
-    SECTION "Genenerate Map Code Data",ROMX
+    SECTION "Genenerate Map Code Data",ROMX[$4000]
+
+;-------------------------------------------------------------------------------
+
+; Aligned to $100
+abs_clamp_array_64: ; returns absolute value up to 63, clamped to 63
+
+VAL SET 0 ; 0 to 63
+    REPT 64
+    DB  VAL
+VAL SET VAL+1
+    ENDR
+
+    REPT 129 ; 64 - 191 (64 to 123, -64 to -128
+    DB  63
+    ENDR
+
+VAL SET 63 ; -63 to -1
+    REPT 63
+    DB  VAL
+VAL SET VAL+(-1) ; WTF, RGBDS, really?
+    ENDR
+
+;-------------------------------------------------------------------------------
+
+; Aligned to $100
+shift_left_six_lsb:
+    DB $00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0
+    DB $00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0
+    DB $00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0
+    DB $00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0,$00,$40,$80,$C0
+
+; 64 bytes after the first one
+shift_left_six_msb:
+    DB $00,$00,$00,$00,$01,$01,$01,$01,$02,$02,$02,$02,$03,$03,$03,$03
+    DB $04,$04,$04,$04,$05,$05,$05,$05,$06,$06,$06,$06,$07,$07,$07,$07
+    DB $08,$08,$08,$08,$09,$09,$09,$09,$0A,$0A,$0A,$0A,$0B,$0B,$0B,$0B
+    DB $0C,$0C,$0C,$0C,$0D,$0D,$0D,$0D,$0E,$0E,$0E,$0E,$0F,$0F,$0F,$0F
+
+;-------------------------------------------------------------------------------
+
+; The right and bottom bounds should have only 0s as the clamped values to 63
+; will read from them.
+gen_map_circle:
+    INCLUDE "gen_map_circle.inc"
+
+;-------------------------------------------------------------------------------
+
+; X and Y are signed 8 bit values
+
+is_inside_circle_4: ; b = x, a = y
+
+    sla     b
+    add     a,a ; sla a
+
+is_inside_circle_8: ; b = x, a = y
+
+    sla     b
+    add     a,a ; sla a
+
+is_inside_circle_16: ; b = x, a = y
+
+    sla     b
+    add     a,a ; sla a
+
+is_inside_circle_32: ; b = x, a = y
+
+    sla     b
+    add     a,a ; sla a
+
+is_inside_circle_64: ; b = x, a = y
+
+    ; Get absolute clamped value
+
+    ld      h,abs_clamp_array_64>>8
+    ld      l,b
+    ld      b,[hl]
+    ld      l,a
+    ld      l,[hl] ; Y = L. Prepare for next step
+
+    ; Get offset
+
+    ld      h,shift_left_six_lsb>>8 ; Y << 6
+    ld      a,[hl] ; LSB
+    set     6,h ; shift_left_six_msb
+    ld      h,[hl] ; MSB
+
+    or      a,b ; or X
+    ld      l,a
+
+    ; Access array
+
+    ld      de,gen_map_circle
+    add     hl,de
+
+    ld      a,[hl]
+
+    ret
 
 ;-------------------------------------------------------------------------------
 
