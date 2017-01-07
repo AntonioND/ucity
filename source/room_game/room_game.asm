6ddd52a7431c10c5ae0ece1bda18cb74085bdff4
AntonioND <antonio_nd@outlook.com>
2017-01-07 03:09:55 +0100

    Add assertion to main VBL handler
    Make sure that the VBL critical code doesn't take too much time and
    is run after the VBL period.

--- source/room_game/room_game.asm
+++ source/room_game/room_game.asm
@@ -1073,6 +1073,13 @@ RoomGameVBLHandler:
     call    refresh_OAM ; update OAM after moving sprites
     call    bg_update_scroll_registers
 
+    ; Assert that the LCD is still in VBL mode!
+    ld      a,[rLY]
+    cp      a,144 ; cy = 1 if n > a
+    jr      nc,.ok
+    ld      b,b ; Breakpoint
+.ok:
+
     ; Set 8x16 or 8x8 sprites
     ld      b,LCDCF_OBJ8
     ld      a,[game_state]
