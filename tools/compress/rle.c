c50483cb81718fe1fe900a3476eb161215d43674
Antonio Niño Díaz <antonio_nd@outlook.com>
2017-05-16 22:53:19 +0100

    Mark source code of `rle.c` as modifed
    This is done to prevent confusion with other versions of the same file.

--- tools/compress/rle.c
+++ tools/compress/rle.c
@@ -15,7 +15,7 @@
 /*--  You should have received a copy of the GNU General Public License     --*/
 /*--  along with this program. If not, see <http://www.gnu.org/licenses/>.  --*/
 /*----------------------------------------------------------------------------*/
-
+/* File modified by Antonio Niño Díaz in 2017                                 */
 /*----------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
